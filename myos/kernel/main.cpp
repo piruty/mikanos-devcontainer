eeadd4050be05a25aaa33740741f59a441338988
piruty <8793641+piruty@users.noreply.github.com>
2022-07-29 07:23:17 +0900

    6.4 ポーリングでマウス入力

--- myos/kernel/main.cpp
+++ myos/kernel/main.cpp
@@ -2,11 +2,21 @@
 #include <cstddef>
 #include <cstdio>
 
+#include <numeric>
+#include <vector>
+
 #include "frame_buffer_config.hpp"
 #include "graphics.hpp"
+#include "mouse.hpp"
 #include "font.hpp"
 #include "console.hpp"
+#include "logger.hpp"
 #include "pci.hpp"
+#include "usb/memory.hpp"
+#include "usb/device.hpp"
+#include "usb/classdriver/mouse.hpp"
+#include "usb/xhci/xhci.hpp"
+#include "usb/xhci/trb.hpp"
 
 // 配置newの演算子の定義
 void operator delete(void *obj) noexcept
@@ -16,45 +26,12 @@ void operator delete(void *obj) noexcept
 const PixelColor kDesktopBGColor{45, 118, 237};
 const PixelColor kDesktopFGColor{255, 255, 255};
 
-const int kMouseCursorWidth = 15;
-const int kMouseCursorHeight = 24;
-// @: マウスカーソルの縁。.: マウスカーソルの内側 = 白く塗りつぶす
-const char mouse_cursor_shape[kMouseCursorHeight][kMouseCursorWidth + 1] = {
-    "@              ",
-    "@@             ",
-    "@.@            ",
-    "@..@           ",
-    "@...@          ",
-    "@....@         ",
-    "@.....@        ",
-    "@......@       ",
-    "@.......@      ",
-    "@........@     ",
-    "@.........@    ",
-    "@..........@   ",
-    "@...........@  ",
-    "@............@ ",
-    "@......@@@@@@@@",
-    "@......@       ",
-    "@....@@.@      ",
-    "@...@ @.@      ",
-    "@..@   @.@     ",
-    "@.@    @.@     ",
-    "@@      @.@    ",
-    "@       @.@    ",
-    "         @.@   ",
-    "         @@@   ",
-};
-
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter *pixel_writer;
 
-// console_buf begin
 char console_buf[sizeof(Console)];
 Console *console;
-// console_buf end
 
-// printk begin
 // printk: カーネル内部からメッセージを出すための関数。
 // カーネル内ならどこでも使える。書式整形の機能を持つ
 int printk(const char *format, ...) // ...: 可変長引数
@@ -70,7 +47,38 @@ int printk(const char *format, ...) // ...: 可変長引数
   console->PutString(s);
   return result;
 }
-// printk end
+
+char mouse_cursor_buf[sizeof(MouseCursor)];
+MouseCursor *mouse_cursor;
+
+void MouseObserver(int8_t displacement_x, int8_t displacement_y)
+{
+  mouse_cursor->MoveRelative({displacement_x, displacement_y});
+}
+
+void SwitchEhci2Xhci(const pci::Device &xhc_dev)
+{
+  bool intel_ehc_exist = false;
+  for (int i = 0; i < pci::num_device; ++i)
+  {
+    if (pci::devices[i].class_code.Match(0x0cu, 0x03u, 0x20u) && 0x8086 == pci::ReadVendorId(pci::devices[i]))
+    {
+      intel_ehc_exist = true;
+      break;
+    }
+  }
+  if (!intel_ehc_exist)
+  {
+    return;
+  }
+
+  uint32_t superspeed_ports = pci::ReadConfReg(xhc_dev, 0xdc); // USB3PRM
+  pci::WriteConfReg(xhc_dev, 0xd8, superspeed_ports);          // USB3_PSSEN
+  uint32_t ehci2xhci_ports = pci::ReadConfReg(xhc_dev, 0xd4);  // XUSB2PRM
+  pci::WriteConfReg(xhc_dev, 0xd0, ehci2xhci_ports);           // XUSB2PR
+  Log(kDebug, "SwitchEhci2Xhci: SS = %02, xHCI = %02x\n",
+      superspeed_ports, ehci2xhci_ports);
+}
 
 // extern "C": C言語形式で関数を定義する
 // C++の定義だと、関数をコンパイルしたときに装飾される（マンダリング）ので、これを防ぐ
@@ -90,7 +98,6 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config)
   const int kFrameWidth = frame_buffer_config.horizontal_resolution;
   const int kFrameHeight = frame_buffer_config.vertical_resolution;
 
-  // #@@begin(draw_desktop)
   FillRectangle(*pixel_writer, {0, 0}, {kFrameWidth, kFrameHeight - 50}, kDesktopBGColor);
   FillRectangle(*pixel_writer, {0, kFrameHeight - 50}, {kFrameWidth, 50}, {1, 8, 17});
   FillRectangle(*pixel_writer, {0, kFrameHeight - 50}, {kFrameWidth / 5, kFrameHeight - 50}, {80, 80, 80});
@@ -99,39 +106,114 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config)
   console = new (console_buf) Console{
       *pixel_writer, kDesktopFGColor, kDesktopBGColor};
   printk("Welcome to MikanOS!\n");
-  // #@@end(draw_desktop)
+  SetLogLevel(kWarn);
 
-  // #@@begin(draw_mouse_cursor)
-  for (int dy = 0; dy < kMouseCursorHeight; ++dy)
+  // mouse_cursor = new (mouse_cursor_buf) MouseCursor{
+  //     pixel_writer, kDesktopBGColor, {300, 200}};
+  mouse_cursor = new (mouse_cursor_buf) MouseCursor{
+    pixel_writer, kDesktopFGColor, {500, 500}
+  };
+
+  auto err = pci::ScanAllBus();
+  Log(kDebug, "ScanAllBus: %s\n", err.Name());
+  for (int i = 0; i < pci::num_device; ++i)
   {
-    for (int dx = 0; dx < kMouseCursorWidth; ++dx)
+    const auto &dev = pci::devices[i];
+    auto vendor_id = pci::ReadVendorId(dev);
+    auto class_code = pci::ReadClassCode(dev.bus, dev.device, dev.function);
+    Log(kDebug, "%d.%d.%d: vend %04x, class %08x, head %02x\n",
+        dev.bus, dev.device, dev.function, vendor_id, class_code, dev.header_type);
+  }
+
+  // #@@range_begin(find_xhc)
+  // Intel 製を優先して xHC を探す
+  pci::Device *xhc_dev = nullptr;
+  for (int i = 0; i < pci::num_device; ++i)
+  {
+    if (pci::devices[i].class_code.Match(0x0cu, 0x03u, 0x30u))
     {
-      if (mouse_cursor_shape[dy][dx] == '@')
+      xhc_dev = &pci::devices[i];
+
+      // 0x8086: intel製PCIデバイスが持つ共通のベンダID
+      if (0x8086 == pci::ReadVendorId(*xhc_dev))
       {
-        pixel_writer->Write(200 + dx, 100 + dy, {0, 0, 0});
-      }
-      else if (mouse_cursor_shape[dy][dx] == '.')
-      {
-        pixel_writer->Write(200 + dx, 100 + dy, {255, 255, 255});
+        break;
       }
     }
   }
-  // #@@end(draw_mouse_cursor)
 
-  // #@@begin(show_devices)
-  auto err = pci::ScanAllBus();
-  printk("ScanAllBus: %s\n", err.Name());
-
-  for (int i = 0; i < pci::num_device; ++i) {
-    const auto& dev = pci::devices[i];
-    auto vendor_id = pci::ReadVendorId(dev.bus, dev.device, dev.function);
-    auto class_code = pci::ReadClassCode(dev.bus, dev.device, dev.function);
-    printk("%d.%d.%d: vend %04x, class %08x, head %02x\n",
-      dev.bus, dev.device, dev.function, vendor_id, class_code, dev.header_type
-    );
+  if (xhc_dev)
+  {
+    Log(kInfo, "xHC has been found: %d.%d.%d\n",
+        xhc_dev->bus, xhc_dev->device, xhc_dev->function);
   }
-  // #@@end(show_devices)
+  // #@@range_end(find_xhc)
+
+  // #@@range_begin(read_bar)
+  // BAR0レジスタの読み取り
+  const WithError<uint64_t> xhc_bar = pci::ReadBar(*xhc_dev, 0);
+  Log(kDebug, "ReadBar: %s\n", xhc_bar.error.Name());
+  // xhc_bar.valueはxHCのレジス歌軍の戦闘を指すアドレスを返す
+  // 下位4ビットがBARのフラグ
+  const uint64_t xhc_mmio_base = xhc_bar.value & ~static_cast<uint64_t>(0xf);
+  Log(kDebug, "xHC mmio_base = %08lx\n", xhc_mmio_base);
+  // #@@range_end(read_bar)
+
+  // #@@range_begin(init_xhc)
+  usb::xhci::Controller xhc{xhc_mmio_base};
+
+  if (0x8086 == pci::ReadVendorId(*xhc_dev))
+  {
+    SwitchEhci2Xhci(*xhc_dev);
+  }
+  {
+    auto err = xhc.Initialize();
+    Log(kDebug, "xhc.Initialize: %s\n", err.Name());
+  }
+
+  Log(kInfo, "xHC starting\n");
+  xhc.Run();
+  // #@@range_end(init_xhc)
+
+  // #@@range_begin(configure_port)
+  usb::HIDMouseDriver::default_observer = MouseObserver;
+
+  for (int i = 1; i <= xhc.MaxPorts(); ++i)
+  {
+    auto port = xhc.PortAt(i);
+    Log(kDebug, "Port %d: IsConnected=%d\n", i, port.IsConnected());
+
+    // 機器が接続されているポートを見つけたら...
+    if (port.IsConnected())
+    {
+      if (auto err = ConfigurePort(xhc, port))
+      {
+        Log(kError, "failed to configure port: %s at %s:%d\n",
+            err.Name(), err.File(), err.Line());
+        continue;
+      }
+    }
+  }
+  // #@@range_end(configure_port)
+
+  // #@@range_begin(receive_event)
+  while (1)
+  {
+    // ProcessEvent: xHCに対して溜まったイベントを処理するように命令
+    if (auto err = ProcessEvent(xhc))
+    {
+      Log(kError, "Error while ProcessEvent: %s at %s:%d\n",
+          err.Name(), err.File(), err.Line());
+    }
+  }
+  // #@@range_end(receive_event)
 
   while (1)
     __asm__("hlt"); // __asm__(): インラインアセンブラ。アセンブリ言語の命令を埋め込む
+}
+
+extern "C" void __cxa_pure_virtual()
+{
+  while (1)
+    __asm__("hlt");
 }
